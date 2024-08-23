91c53747c14b145c68e3acded65dc6e453cb951f
HarshSahu05 <sahuharsh.0506@gmail.com>
2024-08-23 12:18:51 +0530

    condition added in counter project

--- counter-project/Counter/src/App.jsx
+++ counter-project/Counter/src/App.jsx
@@ -11,10 +11,16 @@ function App() {
 
   const  addValue = ()=>{
     //counter = counter + 1;
-    setCounter(counter+1)
+    if(counter<20){
+      setCounter(counter+1)
+    }
+    
   }
   const removeValue = ()=>{
-    setCounter(counter-1)
+    if(counter>0){
+      setCounter(counter-1)
+    }
+    
   }
   return (
     <>

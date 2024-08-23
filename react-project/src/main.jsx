62858de78344c0103f2d8c6f4983e5a409de71ed
HarshSahu05 <sahuharsh.0506@gmail.com>
2024-08-23 11:51:01 +0530

    created element using react

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const user = "harsh"
+const reactElement = React.createElement(
+  'a',
+  {
+    href : 'hyyps://google.com',
+    target: '_black'
+  },
+  'Click me to visit google',
+  user
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  reactElement
 )
